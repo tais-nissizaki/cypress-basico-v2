03d84a273109d07dd41a402f089a00c84dcb6a5e
Taís Nissizaki <tais.nissizaki@gmail.com>
2024-04-05 18:59:33 -0300

    Roll back the app

--- src/script.js
+++ src/script.js
@@ -45,7 +45,7 @@ document.querySelector('button[type="submit"]').addEventListener(
     if (
       !emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
     ) {
-      //return showAndHideErrorMessage()
+      return showAndHideErrorMessage();
     }
     firstNameField.value = "";
     lastNameField.value = "";
